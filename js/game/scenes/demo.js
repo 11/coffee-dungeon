ad77940a050132cad10e10ba80df5b98f9b3ba0f
Doug Rudolph <drudolph914@gmail.com>
2024-03-01 00:07:07 -0500

    adding comment as to why we changed  how we calculated the camera position

--- js/game/scenes/demo.js
+++ js/game/scenes/demo.js
@@ -20,6 +20,10 @@ export default class Demo extends Scene {
     this.spriteRenderer = new SpriteRenderer()
     
     this.camera = new OrthographicCamera()
+    // we think of the world not in relation to the grid's coordinate system,
+    // but instead we imagine that we undid the isometric coordinate system and pyt
+    // it back in an orthognal coordinate system, but kept the isometric scaling (x*1, y*.5)
+    // this will make the mouse logic a bit easier to work out in the future
     this.camera.translate(
       -(Tile.SIZE * 2) * 5,
       -Tile.SIZE * 2,
