b36eff6f8e2fe4b0f09076e1b579597b9af47fa2
Doug Rudolph <drudolph914@gmail.com>
2024-05-22 17:33:54 -0400

    updating docstrings in clamp util

--- js/engine/utils.js
+++ js/engine/utils.js
@@ -1,4 +1,11 @@
 export default class Utils {
+  /**
+   *
+   * @param {Number} value - the value being set
+   * @param {Number} min - the minimum allowed value
+   * @param {Number} max - the maximum allowed value
+   * @returns {Number}
+   */
   static clamp(value, min, max) {
     if (value < min) {
       return min
