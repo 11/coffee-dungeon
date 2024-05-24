55d8442ad76dc8717a7d4f78b9e1b2f41eae9b19
Doug Rudolph <drudolph914@gmail.com>
2024-05-23 23:39:03 -0400

    remove cards 3

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 import Game from './engine/game.js'
 import Battle from './game/scenes/battle.js'
-import CardShop from './game/scenes/card-shop.js'
 
 window.game = new Game()
 game.addScene('battle', new Battle())
-game.addScene('card-shop', new CardShop())
 game.setScene('battle')
 
 game.addImage('dirt-tileset', 'asset-dirt-tileset')
